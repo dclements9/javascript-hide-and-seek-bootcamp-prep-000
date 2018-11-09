1c1f5d2e332d267bdf724cf755e3dd74c68eaa02
dclements <dclements@protonmail.com>
2018-11-09 15:07:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
 
+function increaseRankBy(n){
+
+}
+
+function deepestChild(){
+
+}
